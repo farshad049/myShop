a562c70e2ba5df822127e05ccc2279d78ea6d368
farshad <farshad049@gmail.com>
2022-06-19 14:51:42 +0430

    loading added

--- app/src/main/java/com/example/myshop/ui/loginAndRegister/RegisterFragment.kt
+++ app/src/main/java/com/example/myshop/ui/loginAndRegister/RegisterFragment.kt
@@ -15,10 +15,10 @@ import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 
-class RegisterFragment: BaseFragment(R.layout.fragment_register) {
+class RegisterFragment : BaseFragment(R.layout.fragment_register) {
     private var _binding: FragmentRegisterBinding? = null
     private val binding get() = _binding!!
-    //private lateinit var auth: FirebaseAuth
+    private lateinit var auth: FirebaseAuth
 
     private val viewModel: AuthViewModel by viewModels()
 
@@ -27,7 +27,7 @@ class RegisterFragment: BaseFragment(R.layout.fragment_register) {
         super.onViewCreated(view, savedInstanceState)
         _binding = FragmentRegisterBinding.bind(view)
 
-       // auth = Firebase.auth
+        auth = Firebase.auth
 
         binding.tvLogin.setOnClickListener {
             findNavController().navigate(RegisterFragmentDirections.actionRegisterFragmentToLoginFragment())
@@ -40,88 +40,92 @@ class RegisterFragment: BaseFragment(R.layout.fragment_register) {
         }
 
 
-
-
-
-
     }//FUN
 
     //validate fields
-    private fun validateFields(){
+    private fun validateFields() {
         val name = binding.etEditFirstName.text.toString().trim()
         val lastName = binding.etEditLastName.text.toString().trim()
-        val email= binding.etEditEmail.text.toString().trim()
+        val email = binding.etEditEmail.text.toString().trim()
         val password = binding.etEditPassword.text.toString().trim()
         val confirm = binding.etEditConfirm.text.toString().trim()
 
-         when{
+        when {
             name.isEmpty() -> {
-                binding.etFirstName.error="* please enter your name"
-                binding.etEditFirstName.addTextChangedListener { binding.etFirstName.error=null }
+                binding.etFirstName.error = "* please enter your name"
+                binding.etEditFirstName.addTextChangedListener { binding.etFirstName.error = null }
                 return
-                }
+            }
             lastName.isEmpty() -> {
-                binding.etLastName.error="* please enter your last name"
-                binding.etEditLastName.addTextChangedListener { binding.etLastName.error=null }
+                binding.etLastName.error = "* please enter your last name"
+                binding.etEditLastName.addTextChangedListener { binding.etLastName.error = null }
                 return
-                }
-            email.isEmpty() || !(Patterns.EMAIL_ADDRESS.matcher(email).matches())-> {
-                binding.etEmail.error="* please enter a valid email address"
-                binding.etEditEmail.addTextChangedListener { binding.etEmail.error=null }
+            }
+            email.isEmpty() || !(Patterns.EMAIL_ADDRESS.matcher(email).matches()) -> {
+                binding.etEmail.error = "* please enter a valid email address"
+                binding.etEditEmail.addTextChangedListener { binding.etEmail.error = null }
                 return
-                }
+            }
             password.isEmpty() -> {
-                binding.etPassword.error="* please enter a password"
-                binding.etEditPassword.addTextChangedListener { binding.etPassword.error=null }
+                binding.etPassword.error = "* please enter a password"
+                binding.etEditPassword.addTextChangedListener { binding.etPassword.error = null }
                 return
-                }
-             password.length <8 ||
-                     !password.matches(".*[A-Z].*".toRegex()) ||
-                     !password.matches(".*[a-z].*".toRegex()) ||
-                     !password.matches(".*[0-9].*".toRegex())
-             ->{
-                 binding.etPassword.error="* more than 8 character, include small and big character and number"
-                 binding.etEditPassword.addTextChangedListener { binding.etPassword.error=null }
-                 return
-             }
+            }
+            password.length < 8 ||
+                    !password.matches(".*[A-Z].*".toRegex()) ||
+                    !password.matches(".*[a-z].*".toRegex()) ||
+                    !password.matches(".*[0-9].*".toRegex())
+            -> {
+                binding.etPassword.error =
+                    "* more than 8 character, include small and big character and number"
+                binding.etEditPassword.addTextChangedListener { binding.etPassword.error = null }
+                return
+            }
             confirm.isEmpty() -> {
-                binding.etConfirm.error="* please confirm password"
-                binding.etEditConfirm.addTextChangedListener { binding.etConfirm.error=null }
+                binding.etConfirm.error = "* please confirm password"
+                binding.etEditConfirm.addTextChangedListener { binding.etConfirm.error = null }
                 return
-                }
+            }
             confirm != password -> {
-                binding.etConfirm.error="* password Doesn't match"
-                binding.etEditConfirm.addTextChangedListener { binding.etConfirm.error=null }
+                binding.etConfirm.error = "* password Doesn't match"
+                binding.etEditConfirm.addTextChangedListener { binding.etConfirm.error = null }
                 return
-                }
+            }
             !binding.checkAgree.isChecked -> {
-                Snackbar.make(mainActivity.findViewById(android.R.id.content),"please check the agreement", Snackbar.LENGTH_LONG).show()
+                Snackbar.make(
+                    mainActivity.findViewById(android.R.id.content),
+                    "please check the agreement",
+                    Snackbar.LENGTH_LONG
+                ).show()
                 return
             }
             else -> {
-
-                viewModel.register(email,password)
+                showProgressBar()
+                viewModel.register(email, password)
                 viewModel.userData.observe(viewLifecycleOwner) {
+                    dismissProgressBar()
                     if (it != null) {
                         findNavController().navigate(RegisterFragmentDirections.actionRegisterFragmentToLoginFragment())
-                    }else{
-                        Toast.makeText(requireContext(),"registration failed",Toast.LENGTH_LONG).show()
+                    } else {
+                        Toast.makeText(requireContext(), "registration failed", Toast.LENGTH_LONG)
+                            .show()
                     }
 
+//                showProgressBar()
 //                auth.createUserWithEmailAndPassword(email,password).addOnCompleteListener {
+//                    dismissProgressBar()
 //                    if (it.isSuccessful){
 //                            findNavController().navigate(RegisterFragmentDirections.actionRegisterFragmentToLoginFragment())
 //
 //                    }else{
 //                        Toast.makeText(requireContext(),"registration failed",Toast.LENGTH_LONG).show()
 //                    }
-               }
+//               }
+
+                }
             }
         }
     }
-
-
-
 
 
 
